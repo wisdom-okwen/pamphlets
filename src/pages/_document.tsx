fa5839f931057ff4c45767f56bfcbd382f448f45
Wisdom Okwen <wisdomokwen@gmail.com>
2025-12-17 15:37:17 -0500

    dns verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en" className="scrollbar-gutter-stable">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="X5NeDfA5p10G_SOucGsO4nGnKNJYaabWuxCfBedSaVY" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
