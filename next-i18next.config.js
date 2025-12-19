6b9a09bd25c0f390ebf1a1fe986dadbc623e3b03
Wisdom Okwen <wisdomokwen@gmail.com>
2025-12-19 03:06:01 -0500

    Resolve translation path issues

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next-i18next').UserConfig} */
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es', 'fr', 'sw', 'zh', 'ja'],
     localeDetection: false,
   },
-  localePath: typeof window === 'undefined' 
-    ? require('path').resolve('./public/locales')
-    : '/locales',
+  localePath: path.resolve('./public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
